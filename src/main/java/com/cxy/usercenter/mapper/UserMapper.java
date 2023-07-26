88ff9365989b8b9f8409af5f52efa8c3f565c5c9
Cabbage-xy <cxy4915@gmail.com>
2023-07-26 23:00:47 +0800

    init project

---
+++ src/main/java/com/cxy/usercenter/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.cxy.usercenter.mapper;
+
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.cxy.usercenter.model.User;
+
+//BaseMapper是Mybatis Plus提供的基类
+public interface UserMapper extends BaseMapper<User> {
+
+}
