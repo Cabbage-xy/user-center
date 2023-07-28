c520232aa597e53376327200fabd1dccc8e49124
Cabbage-xy <cxy4915@gmail.com>
2023-07-28 21:46:31 +0800

    refactor: remove unused import

--- src/test/java/com/cxy/usercenter/SampleTest.java
+++ src/test/java/com/cxy/usercenter/SampleTest.java
@@ -3,7 +3,6 @@ package com.cxy.usercenter;
 import com.cxy.usercenter.mapper.UserMapper;
 import com.cxy.usercenter.model.User;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
