d1cc2a1d54084d6592f238161295e6f1d16cf30f
Cabbage-xy <cxy4915@gmail.com>
2023-08-12 22:03:30 +0800

    refactor: add line 'role' to table user

--- init.sql
+++ init.sql
@@ -1,19 +1,21 @@
 DROP TABLE IF EXISTS user;
 
+-- auto-generated definition
 create table user
 (
-    id           bigint auto_increment,
-    username     varchar(256) null comment '用户昵称',
-    userAccount  varchar(256) null comment '账号',
-    avatarUrl    varchar(1024) null comment '用户头像',
-    gender       tinyint null comment '性别',
-    userPassword varchar(256)       not null comment '密码',
-    phone        varchar(128) null comment '电话',
-    email        varchar(512) null comment '邮箱',
-    userStatus   tinyint null comment '状态',
+    id           bigint auto_increment
+        primary key,
+    username     varchar(256)                       null comment '用户昵称',
+    userAccount  varchar(256)                       null comment '账号',
+    avatarUrl    varchar(1024)                      null comment '用户头像',
+    gender       tinyint                            null comment '性别',
+    userPassword varchar(256)                       not null comment '密码',
+    phone        varchar(128)                       null comment '电话',
+    email        varchar(512)                       null comment '邮箱',
+    userStatus   tinyint                            null comment '状态',
     createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
     updateTime   datetime default CURRENT_TIMESTAMP null comment '更新时间',
-    isDelete     tinyint  default 0 not null comment '是否删除',
-    constraint user_pk
-        primary key (id)
+    isDelete     tinyint  default 0                 not null comment '是否删除',
+    role         int      default 0                 null comment '用户角色 0 - 普通用户 1 - 管理员'
 );
+
